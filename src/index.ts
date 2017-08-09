08846b89dd8ca64e54d1558f73bed58614220bff
Dan DeGreef <daniel_degreef@condenast.com>
2017-08-09 10:57:53 -0500

    add url handlers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import {IncomingMessage, Server, ServerResponse} from 'http';
-import Router, {Route} from './router';
+import Router, {Ping, Route, Home} from './router';
 
 const server = new Server();
 const router = new Router();
 
+router.register({route: new Ping(), method: "GET", pattern: "/ping"});
+router.register({route: new Home(), method: "GET", pattern: "/"});
+
 server.on('request', async (request: IncomingMessage, response: ServerResponse) => {
-  const route: Route = await router.match(request, response);
+  const route: Route = await router.match(request);
   await route.handle(request, response);
   response.end();
 });
--- src/router.ts
+++ src/router.ts
@@ -1,22 +1,61 @@
 import {IncomingMessage, ServerResponse} from "http";
 
 export default class Router {
-  match(request: IncomingMessage, response: ServerResponse): Promise<Route> {
-    return new Promise(resolve => {
-      resolve(new Ping());
+  private registeredRoutes: Array<RouteRegistrationInfo>;
+
+  constructor() {
+    this.registeredRoutes = [];
+  }
+
+  register(routeInfo: RouteRegistrationInfo) {
+    this.registeredRoutes.push(routeInfo);
+  }
+
+  match(request: IncomingMessage): Promise<Route> {
+
+    return new Promise((resolve, reject) => {
+      const matchedRouteInfo: RouteRegistrationInfo | undefined = this.registeredRoutes.find(routeInfo => {
+        return (routeInfo.method === request.method && routeInfo.pattern === request.url)
+      });
+
+      if (matchedRouteInfo) {
+        resolve(matchedRouteInfo.route);
+      } else {
+        reject(new Error('bad url'));
+      }
+
+
     });
   }
 }
 
+export type RouteRegistrationInfo = Readonly<{
+  route: Route;
+  method: string;
+  pattern: string;
+}>
+
 export interface Route {
   handle(request: IncomingMessage, response: ServerResponse): Promise<ServerResponse>;
+
 }
 
-class Ping implements Route {
+export class Ping implements Route {
   handle(request: IncomingMessage, response: ServerResponse): Promise<ServerResponse> {
     return new Promise((resolve) => {
       response.write('pong');
       resolve(response);
     });
   }
+
+}
+
+export class Home implements Route {
+  handle(request: IncomingMessage, response: ServerResponse): Promise<ServerResponse> {
+    return new Promise((resolve) => {
+      response.write('Hello World!');
+      resolve(response);
+    });
+  }
+
 }
