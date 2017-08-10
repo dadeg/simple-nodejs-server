4ce3792959a5510597c63a6f8b62578bc2c02f92
Dan DeGreef <daniel_degreef@condenast.com>
2017-08-09 22:19:56 -0500

    fix bug in return

--- src/routes/crud-route.ts
+++ src/routes/crud-route.ts
@@ -3,13 +3,11 @@ import {IncomingMessage, ServerResponse} from "http";
 
 export abstract class CrudRoute implements Route {
   handle(request: IncomingMessage, response: ServerResponse): Promise<ServerResponse> {
-    if (request.method === "GET") {
-      return this.get(request, response);
-    }
-    
     if (request.method === "POST") {
       return this.post(request, response);
     }
+
+    return this.get(request, response);
   }
 
   get(request: IncomingMessage, response: ServerResponse): Promise<ServerResponse> {
